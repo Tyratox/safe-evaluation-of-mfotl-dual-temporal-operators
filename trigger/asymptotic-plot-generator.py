fda0b8c8222aca6dcfb286c098f045f20506f54f
Nico Hauser <me@tyratox.ch>
2021-08-03 22:02:47 +0200

    update plot generation scripts

--- trigger/asymptotic-plot-generator.py
+++ trigger/asymptotic-plot-generator.py
@@ -19,6 +19,11 @@ def index_to_key(idx):
     return idx.map(asymptotic_order.index)
 
 
+def err(correct, approximation):
+    x = np.average((correct - approximation) ** 2)
+    return x
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--measurements',
                     help='The measurement csv', required=True)
@@ -88,9 +93,9 @@ for experiment_name in experiments:
     nExps = baseline3Index - baseline2Index
     cExps = len(labels) - baseline3Index
 
-    x_l = np.array(list(range(lExps)))
-    x_n = np.array(list(range(nExps)))
-    x_c = np.array(list(range(cExps)))
+    x_l = np.array(list(range(lExps))) + 1
+    x_n = np.array(list(range(nExps))) + 1
+    x_c = np.array(list(range(cExps))) + 1
 
     # native_means = df['native real time']['mean'].to_numpy()
     native_meval_means = exp['native meval time']['mean'].to_numpy()
@@ -100,41 +105,46 @@ for experiment_name in experiments:
     native_meval_stds = exp['native meval time']['std'].to_numpy()
     native_mmtaux_stds = exp['native trigger time']['std'].to_numpy()
 
-    A_l = np.vstack([x_l, np.ones(len(x_l))]).T
+    A_l = np.vstack([np.log(x_l) * x_l, np.ones(len(x_l))]).T
     a_l, b_l = np.linalg.lstsq(
         A_l, native_mmtaux_means[0:baseline2Index], rcond=None)[0]
 
-    A_n = np.vstack([x_n, np.ones(len(x_n))]).T
+    A_n = np.vstack([np.log(x_n) * x_n, np.ones(len(x_n))]).T
     a_n, b_n = np.linalg.lstsq(
         A_n, native_mmtaux_means[baseline2Index:baseline3Index], rcond=None)[0]
 
-    A_c = np.vstack([x_c, np.ones(len(x_c))]).T
+    A_c = np.vstack([np.log(x_c) * x_c, np.ones(len(x_c))]).T
     a_c, b_c = np.linalg.lstsq(
         A_c, native_mmtaux_means[baseline3Index:], rcond=None)[0]
 
-    linearFunctionX_l = np.linspace(0, baseline2Index-1, 100)
-    linearFunctionY_l = a_l * linearFunctionX_l + b_l  # start at x = 0, a * x + b
+    linearFunctionX_l = np.linspace(1, baseline2Index, 100)
+    # start at x = 0, a * x + b
+    linearFunctionY_l = np.log(linearFunctionX_l) * \
+        a_l * linearFunctionX_l + b_l
+    lin_y_l = np.log(x_l) * a_l * x_l + b_l
 
-    linearFunctionX_n = np.linspace(0, baseline3Index-baseline2Index-1, 100)
+    linearFunctionX_n = np.linspace(1, baseline3Index-baseline2Index, 100)
     # start at x = baseline2Index, a * x + b
-    linearFunctionY_n = a_n * (linearFunctionX_n) + b_n
+    linearFunctionY_n = np.log(linearFunctionX_n) * \
+        a_n * (linearFunctionX_n) + b_n
+    lin_y_n = np.log(x_n) * a_n * x_n + b_n
 
-    linearFunctionX_c = np.linspace(0, len(labels)-baseline3Index-1, 100)
+    linearFunctionX_c = np.linspace(1, len(labels)-baseline3Index, 100)
     # start at x = baseline3Index, a * x + b
-    linearFunctionY_c = a_c * (linearFunctionX_c) + b_c
+    linearFunctionY_c = np.log(linearFunctionX_c) * \
+        a_c * (linearFunctionX_c) + b_c
+    lin_y_c = np.log(x_c) * a_c * x_c + b_c
 
-    B_c = np.vstack([x_c*x_c, x_c, np.ones(len(x_c))]).T
+    B_c = np.vstack([np.log(x_c) * (x_c ** 2), np.log(x_c)
+                     * x_c, np.ones(len(x_c))]).T
     m_c, p_c, q_c = np.linalg.lstsq(
         B_c, native_mmtaux_means[baseline3Index:], rcond=None)[0]
 
-    quadraticFunctionX_c = np.linspace(0, len(labels)-baseline3Index-1, 100)
-    quadraticFunctionY_c = m_c * ((quadraticFunctionX_c) ** 2) + p_c * (quadraticFunctionX_c) + \
+    quadraticFunctionX_c = np.linspace(1, len(labels)-baseline3Index, 100)
+    quadraticFunctionY_c = m_c * np.log(quadraticFunctionX_c) * ((quadraticFunctionX_c) ** 2) + p_c * (np.log(quadraticFunctionX_c) * quadraticFunctionX_c) + \
         q_c  # start at x = 2, a * x^2 + b * x + c
-
-    # the label locations
-    x1 = np.arange(baseline2Index)
-    x2 = np.arange(baseline3Index - baseline2Index)
-    x3 = np.arange(len(labels) - baseline3Index)
+    quad_y_c = m_c * np.log(x_c) * ((x_c) ** 2) + \
+        p_c * (np.log(x_c) * x_c) + q_c
 
     # the width of the bars
     width = 0.35
@@ -142,56 +152,58 @@ for experiment_name in experiments:
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
     # rects_native = ax.bar(x + width/2, native_means, width,
     #                       label='native', yerr=native_stds, ecolor='black', capsize=2, color='#e67e22')
-    ax1.bar(x1, native_meval_means[0:baseline2Index], width, label='meval', yerr=native_meval_stds[0:baseline2Index],
+    ax1.bar(x_l, native_meval_means[0:baseline2Index], width, label='meval', yerr=native_meval_stds[0:baseline2Index],
             ecolor='black', capsize=2, color='tab:orange')  # , color='#d35400'
-    ax1.bar(x1, native_mmtaux_means[0:baseline2Index], 0.75*width, label='mmtaux', yerr=native_mmtaux_stds[0:baseline2Index],
+    ax1.bar(x_l, native_mmtaux_means[0:baseline2Index], 0.75*width, label='mmtaux', yerr=native_mmtaux_stds[0:baseline2Index],
             ecolor='black', capsize=2, color='tab:green')  # , color='#2ecc71'
 
     ax1.set_ylim(bottom=0)
     ax1.plot(linearFunctionX_l, linearFunctionY_l, 'black', linestyle='dashed')
-    custom_lines1 = [Line2D([0], [0], color="black", lw=1, linestyle='dashed')]
-    ax1.legend(custom_lines1, [
-        str(round(a_l, 1)) + r'$\: \cdot \: x \: ' +
-        (r'+' if b_l >= 0 else r'-') + str(round(abs(b_l), 1)) + r'$'])
 
-    ax1.set_xticks(x1)
+    custom_lines1 = [Line2D([0], [0], color="black", lw=1, linestyle='dashed')]
+    legend1 = [
+        f'linear, MSE: {err(native_meval_means[0:baseline2Index], lin_y_l):.2}']
+    ax1.legend(custom_lines1, legend1)
+
+    ax1.set_xticks(x_l)
     ax1.set_xticklabels(
         ["baseline" if ("baseline" in a) else map_label(a) for a in labels[0:baseline2Index]])
 
-    ax2.bar(x2, native_meval_means[baseline2Index:baseline3Index], width, label='meval', yerr=native_meval_stds[baseline2Index:baseline3Index],
+    ax2.bar(x_n, native_meval_means[baseline2Index:baseline3Index], width, label='meval', yerr=native_meval_stds[baseline2Index:baseline3Index],
             ecolor='black', capsize=2, color='tab:orange')  # , color='#d35400'
-    ax2.bar(x2, native_mmtaux_means[baseline2Index:baseline3Index], 0.75*width, label='mmtaux', yerr=native_mmtaux_stds[baseline2Index:baseline3Index],
+    ax2.bar(x_n, native_mmtaux_means[baseline2Index:baseline3Index], 0.75*width, label='mmtaux', yerr=native_mmtaux_stds[baseline2Index:baseline3Index],
             ecolor='black', capsize=2, color='tab:green')  # , color='#2ecc71'
 
     ax2.set_ylim(bottom=0)
     ax2.plot(linearFunctionX_n, linearFunctionY_n, 'black', linestyle='dashed')
+
     custom_lines2 = [Line2D([0], [0], color="black", lw=1, linestyle='dashed')]
-    ax2.legend(custom_lines1, [
-        str(round(a_n, 1)) + r'$ \: \cdot \: x \:' +
-        (r'+' if b_n >= 0 else r'-') + str(round(abs(b_n), 1)) + r'$'])
-    ax2.set_xticks(x2)
+    legend2 = [
+        f'linear, MSE: {err(native_meval_means[baseline2Index:baseline3Index], lin_y_n):.2}']
+
+    ax2.legend(custom_lines2, legend2)
+    ax2.set_xticks(x_n)
     ax2.set_xticklabels(
         ["baseline" if ("baseline" in a) else map_label(a) for a in labels[baseline2Index:baseline3Index]])
 
-    ax3.bar(x3, native_meval_means[baseline3Index:], width, label='meval', yerr=native_meval_stds[baseline3Index:],
+    ax3.bar(x_c, native_meval_means[baseline3Index:], width, label='meval', yerr=native_meval_stds[baseline3Index:],
             ecolor='black', capsize=2, color='tab:orange')  # , color='#d35400'
-    ax3.bar(x3, native_mmtaux_means[baseline3Index:], 0.75*width, label='mmtaux', yerr=native_mmtaux_stds[baseline3Index:],
+    ax3.bar(x_c, native_mmtaux_means[baseline3Index:], 0.75*width, label='mmtaux', yerr=native_mmtaux_stds[baseline3Index:],
             ecolor='black', capsize=2, color='tab:green')  # , color='#2ecc71'
 
     ax3.set_ylim(bottom=0)
     ax3.plot(linearFunctionX_c, linearFunctionY_c, 'black', linestyle='dashed')
     ax3.plot(quadraticFunctionX_c, quadraticFunctionY_c,
-             'silver', linestyle='dashed')
+             'black', linestyle='dotted')
     custom_lines3 = [Line2D([0], [0], color="black", lw=1, linestyle='dashed'),
-                     Line2D([0], [0], color="silver", lw=1, linestyle='dashed')]
-    ax3.legend(custom_lines3, [
-        str(round(a_c, 1)) + r'$ \: \cdot \: x \:' +
-        (r'+' if b_c >= 0 else r'-') + str(round(abs(b_c), 1)) + r'$',
-        str(round(m_c, 1)) + r'$ \: \cdot \: x^2 \:' +
-        (r'+' if p_c >= 0 else r'-') + str(round(abs(p_c), 1)) + r'\cdot x' +
-        (r'+' if q_c >= 0 else r'-') + str(round(abs(q_c), 1)) + r'$',
-    ], loc='upper left')
-    ax3.set_xticks(x3)
+                     Line2D([0], [0], color="black", lw=1, linestyle='dotted')
+                     ]
+    legend3 = [
+        f'linear, MSE: {err(native_meval_means[baseline3Index:], lin_y_c):.2}',
+        f'quadratic, MSE: {err(native_meval_means[baseline3Index:], quad_y_c):.2}',
+    ]
+    ax3.legend(custom_lines3, legend3, loc='upper left')
+    ax3.set_xticks(x_c)
     ax3.set_xticklabels(
         ["baseline" if ("baseline" in a) else map_label(a) for a in labels[baseline3Index:]])
 
